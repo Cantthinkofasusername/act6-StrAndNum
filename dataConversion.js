7248cb061c4d2e36264d3b4c0afb79e7648075e8
Your Name <shado@LAPTOP-U7GG9IKM>
2024-09-15 12:43:42 +0800

    initial commit

---
+++ dataConversion.js
@@ -0,0 +1,11 @@
+//Convert a String to a Number
+let price = "200";
+console.log(parseInt(price));
+
+//Convert a Number to a String
+let distance = 150;
+console.log(String(distance));
+
+//Convert a String to a Decimal Number
+let weight = "72.5";
+console.log(parseFloat(weight))
